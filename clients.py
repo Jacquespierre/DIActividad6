8c60b02ed8658683709e7178d9cbd12a601325ec
Jacquespierre <73559639+Jacquespierre@users.noreply.github.com>
2022-01-20 19:44:51 +0100

    Initial commit

---
+++ clients.py
@@ -0,0 +1,38 @@
+import var
+
+
+class Clientes():
+    def validarDNI():
+        try:
+            dni = var.ui.CampoDNI.text()
+            var.ui.CampoDNI.setText(dni.upper())
+            tabla = 'TRWAGMYFPDXBNJZSQVHLCKE'  # letras dni
+            dig_ext = 'XYZ'  # digito
+            reemp_dig_ext = {'X': '0', 'Y': '1', 'Z': '2'}
+            numeros = '1234567890'
+            dni = dni.upper()  # conver la letra mayusculas
+            if len(dni) == 9:
+                dig_control = dni[8]
+                dni = dni[:8]
+                if dni[0] in dig_ext:
+                    dni = dni.replace(dni[0], reemp_dig_ext[dni[0]])
+                if len(dni) == len([n for n in dni if n in numeros]) and tabla[int(dni) % 23] == dig_control:
+                    var.ui.label.setStyleSheet('QLabel {color: green;}')
+                    var.ui.label.setText('V')
+                else:
+                    var.ui.label.setStyleSheet('QLabel {color: red;}')
+                    var.ui.label.setText('X')
+            else:
+                var.ui.label.setStyleSheet('QLabel {color: red;}')
+                var.ui.label.setText('X')
+        except Exception as error:
+            print('Error en modulo validar DNI', error)
+
+    def selSexo():
+        try:
+            if var.ui.radioButtonFem.isChecked():
+                print('Marcado femenino')
+            if var.ui.radioButtonMas.isChecked():
+                print('Marcado masculino')
+        except Exception as error:
+            print('Error en módulo seccionar sexo:', error)
